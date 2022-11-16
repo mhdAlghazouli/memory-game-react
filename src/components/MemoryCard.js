2910098503da71bec590534bf0dacaa9e8fe67a1
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-11-15 19:17:30 -0500

    Part-1

---
+++ src/components/MemoryCard.js
@@ -0,0 +1,10 @@
+import "./MemoryCard.css"
+const MemoryCard = () => {
+  return ( 
+    <div className="MemoryCard">
+      <img src="https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg" alt="DCLogo" />
+    </div>
+   );
+}
+ 
+export default MemoryCard;
