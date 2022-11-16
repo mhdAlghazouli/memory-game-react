38324af05b4c5c7774ee2fdc58efe5280956ce2e
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-11-15 19:44:11 -0500

    Part-2

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./MemoryCard.css"
 const MemoryCard = () => {
+  const [isFlipped, setIsFlipped] = useState(false)
+  function clickHandler() {
+    setIsFlipped(true)
+  }
   return ( 
-    <div className="MemoryCard">
-      <img src="https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg" alt="DCLogo" />
+    <div className="MemoryCard" onClick={clickHandler}>
+      <div className={isFlipped  ? "MemoryCardInner flipped" : "MemoryCardInner"}>
+        <div className="MemoryCardBack">
+          <img src="https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg" alt="DCLogo" />
+        </div>
+        <div className="MemoryCardFront">∆</div>
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default MemoryCard;
